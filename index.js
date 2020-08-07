8c9c93878278e69226ad4a17c7906448672a00c7
juanpms2 <jpablo69@hotmail.es>
2020-08-07 09:58:39 +0200

    update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var app = express();
 
 app.get("/", function (req, res) {
 	console.log(req);
-	res.send("Hello world!!!");
+	res.send("Hello world!!! Docker + AWS + travis + terraform deploy");
 });
 
 app.get("/:name", function (req, res) {
