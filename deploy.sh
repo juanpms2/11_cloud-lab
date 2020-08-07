373c615d211997d9f2b520d248315a8021f589aa
juanpms2 <jpablo69@hotmail.es>
2020-08-07 20:54:27 +0200

    prueba deploy

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,1 @@
-ssh -i $KEY $USER@$HOST \
-    "sudo yum install docker -y && service docker start && sudo docker run --rm -d -p 80:8888 jpabloms2/masterlab"
+sudo yum install docker -y && service docker start && sudo docker run --rm -d -p 80:8888 jpabloms2/masterlab
