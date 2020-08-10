315a7da95bd7ec7c49449aacbbf16ddd64a0c958
juanpms2 <jpablo69@hotmail.es>
2020-08-10 08:13:20 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,7 @@
-sudo yum install docker -y && service docker start && sudo docker run --rm -d -p 80:8888 jpabloms2/masterlab
+echo "Kill container..."
+sudo docker kill jpabloms2/masterlab
+echo "Pull imagen docker hub"
+sudo docker pull -t jpabloms2/masterlemoncode_cloud
+echo "run new container"
+sudo docker run --rm -d -p 80:8888 --name jpabloms2/masterlab jpabloms2/masterlemoncode_cloud
+echo "finish update"
