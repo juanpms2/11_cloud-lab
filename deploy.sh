1a1673e1b68550831235bf5dbf5d80453368bfaa
juanpms2 <jpablo69@hotmail.es>
2020-08-11 21:34:53 +0200

    666 ok :-)

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 echo "Kill container..."
-sudo docker kill masterlab
+docker kill masterlab
+echo "delete docker image"
+docker rmi -f jpabloms2/masterlemoncode_cloud 
 echo "run new container"
 sudo docker run --rm -d -p 80:8888 --name masterlab jpabloms2/masterlemoncode_cloud
 echo "finish update"
